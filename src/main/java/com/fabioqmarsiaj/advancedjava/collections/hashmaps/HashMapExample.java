1575b74d522511bd6f4947c2fedd9607981b6995
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2023-03-03 15:20:34 -0300

    HashMap example

---
+++ src/main/java/com/fabioqmarsiaj/advancedjava/collections/hashmaps/HashMapExample.java
@@ -0,0 +1,26 @@
+package com.fabioqmarsiaj.advancedjava.collections.hashmaps;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<String, Integer> phonebook = new HashMap<>();
+        /*
+            ! HashMaps does not allow duplicated entries
+            ! so in this case the entry 'Brenda' is overwritten
+
+            * We can also assign null as an entry value
+         */
+
+        phonebook.put("Kevin", 12345);
+        phonebook.put("Jill", 98765);
+        phonebook.put("Brenda", 123123);
+        phonebook.put("Brenda", 22222);
+        phonebook.put(null, 000);
+        if(phonebook.containsKey("Brenda")) {
+            phonebook.remove("Brenda");
+        }
+        phonebook.clear();
+        System.out.println(phonebook);
+    }
+}
