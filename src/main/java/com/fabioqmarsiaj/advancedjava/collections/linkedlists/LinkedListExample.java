1ac0591961a060252148a38baff8d646add76c2b
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2023-03-03 15:09:34 -0300

    Queue implementation on LinkedList

--- src/main/java/com/fabioqmarsiaj/advancedjava/collections/linkedlists/LinkedListExample.java
+++ src/main/java/com/fabioqmarsiaj/advancedjava/collections/linkedlists/LinkedListExample.java
@@ -1,13 +1,13 @@
-package com.fabioqmarsiaj.advancedjava.collections;
+package com.fabioqmarsiaj.advancedjava.collections.linkedlists;
 
 import java.util.LinkedList;
 
 public class LinkedListExample {
     public static void main(String[] args) {
 
-        // * LinkedList are faster to add/remove elements than an ArrayList, but they are heavier
+        // * LinkedList are faster to add/remove elements in the middle than an ArrayList, but they are heavier
         // * since each element stores a reference to the previous and next element of the List.
-        // * ArrayList are faster to get elements than LinkedList
+        // * ArrayLists are faster to get elements than LinkedList
         LinkedList<String> myList = new LinkedList<>();
         myList.add("A");
         myList.add("B");
---
+++ src/main/java/com/fabioqmarsiaj/advancedjava/collections/queue/Store.java
@@ -0,0 +1,22 @@
+package com.fabioqmarsiaj.advancedjava.collections.queue;
+
+import java.util.LinkedList;
+
+public class Store {
+    public static void main(String[] args) {
+        LinkedList<Customer> queue = new LinkedList<>();
+        queue.add(new Customer("Sally"));
+        queue.add(new Customer("Ben"));
+        queue.add(new Customer("Emma"));
+        queue.add(new Customer("Fred"));
+        System.out.println(queue);
+        serveCustomer(queue);
+        System.out.println(queue);
+    }
+
+    static void serveCustomer(LinkedList<Customer> queue){
+        // ! Takes the LinkedList as a parameter and removes the FIRST value
+        Customer c = queue.poll();
+        c.serve();
+    }
+}
