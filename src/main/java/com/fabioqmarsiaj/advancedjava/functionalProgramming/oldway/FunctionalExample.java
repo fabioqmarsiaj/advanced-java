d2610eb565ac7fbeb07d367f149efc3ecf5ff50c
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-24 14:26:06 -0300

    Rearranging comments on code

--- src/main/java/com/fabioqmarsiaj/advancedjava/functionalProgramming/oldway/FunctionalExample.java
+++ src/main/java/com/fabioqmarsiaj/advancedjava/functionalProgramming/oldway/FunctionalExample.java
@@ -3,20 +3,22 @@ package com.fabioqmarsiaj.advancedjava.functionalProgramming.oldway;
 public class FunctionalExample {
     public static void main(String[] args) {
 
-        // ! This is known as Anonymous Inner Class
+        /*
+            ! This is known as Anonymous Inner Class
+            ! Lambdas were introduced to improve this messy code
+         */
         GreetingMessage greetingMessage = new GreetingMessage() {
             @Override
             public void greet(String name) {
                 System.out.println("Hi, " + name);
             }
         };
-
-        /* ! Allows java to pass code as DATA
-         * Which means 'to treat functionality as method argument'
-         ! Lambdas were introduced to improve this messy code
-         */
         greetingMessage.greet("Fabio");
 
+        /*
+            ! Allows java to pass code as DATA
+            ! Which means 'to treat functionality as method argument'
+         */
         GreetingMessage greetingMessage2 = name -> System.out.println(name);
         greetingMessage2.greet("Teste");
 
