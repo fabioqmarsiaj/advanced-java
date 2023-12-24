9f94758c30fedcb5e96e0f488d3b91090f21d3b5
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-24 14:19:41 -0300

    Creating greetingMessage FI without boilerplate code

--- src/main/java/com/fabioqmarsiaj/advancedjava/functionalProgramming/oldway/FunctionalExample.java
+++ src/main/java/com/fabioqmarsiaj/advancedjava/functionalProgramming/oldway/FunctionalExample.java
@@ -12,9 +12,13 @@ public class FunctionalExample {
         };
 
         /* ! Allows java to pass code as DATA
-         * Which mean 'to treat functionality as method argument'
+         * Which means 'to treat functionality as method argument'
          ! Lambdas were introduced to improve this messy code
          */
         greetingMessage.greet("Fabio");
+
+        GreetingMessage greetingMessage2 = name -> System.out.println(name);
+
+        greetingMessage2.greet("Teste");
     }
 }
